da9f1e085f84de4cd0fe2875ba76e18acfa605bd
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-08-28 13:47:06 +0530

    basic react app files

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ecommerce-website
+
+require (
+	github.com/dustin/go-humanize v1.0.0
+	github.com/gin-gonic/gin v1.4.0
+	github.com/google/uuid v1.1.1
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.1.1
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr := "user=postgres dbname = ecommerce_website host=localhost password=test1234 sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	router := gin.Default()
+	http.Handle("/", router)
+
+	http.ListenAndServe(":8000", nil)
+
+}
