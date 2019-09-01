4f2b3a13980fb0bf142e8139b1c9379c0132c504
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-09-01 23:48:30 +0530

    post user and required api has been created in main.go

--- main.go
+++ main.go
@@ -10,6 +10,46 @@ import (
 	"net/http"
 )
 
+type User struct {
+	ID          string    `json:"id,omiempty`
+	Username    string    `json:"username,omiempty"`
+	Password    string    `json:"password,omiempty"`
+	Age         int       `json:"age,omiempty"`
+	Sex         string    `json:"sex,omiempty"`
+	Phonenumber string    `json:"phonenumber,omiempty"`
+	Email       string    `json:"email,omiempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+}
+
+func CreateUser(db *sql.DB, user User) error {
+
+	_, err = db.Exec(`INSERT INTO users(username,email,password,phonenumber)
+									VALUES($1,$2,$3,$4)`,
+
+		user.Username,
+		user.Email,
+		user.password,
+		user.Phonenumber)
+}
+
+func postUserHandler(c *gin.Context, db *sql.DB) {
+
+	user := User{}
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	err = CreateUser(db, user)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.Status(http.StatusCreated)
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -32,6 +72,8 @@ func main() {
 
 	router := gin.Default()
 	http.Handle("/", router)
+
+	router.POST("/user/signup", func(c *gin.context) { postUserHandler(c, db) })
 
 	http.ListenAndServe(":8000", nil)
 	http.ListenAndServe(":8000/signin", nil)
