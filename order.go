50b019464710366e394ecbbc91a3b5ab97f83007
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-09-18 16:36:30 +0530

    order createdAt is added using migrations

--- order.go
+++ order.go
@@ -52,7 +52,7 @@ func postOrderHandler(c *gin.Context, db *sql.DB) {
 		}
 	}
 
-	_, err := db.Exec(`DELETE FROM cart  WHERE user_id= $1`, userId)
+	_, err = db.Exec(`DELETE FROM cart  WHERE user_id= $1`, userId)
 	if err != nil {
 		return
 	}
