3c92ca6265449e5aefbca03bab44abedf77b7e65
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-09-18 15:56:31 +0530

    Get order is separated from main file and authorization has been changed get order function.

--- order.go
+++ order.go
@@ -53,5 +53,62 @@ func postOrderHandler(c *gin.Context, db *sql.DB) {
 	}
 
 	c.Status(http.StatusCreated)
+}
 
+func getOrdersHandler(c *gin.Context, db *sql.DB) {
+
+	userId, err := authorization(c, db)
+	if err != nil {
+		return
+	}
+
+	rows, err := db.Query(`SELECT id FROM orders WHERE user_id=$1`, userId)
+	if err != nil {
+		fmt.Println(err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	var orderId int
+	orderIds := []int{}
+	for rows.Next() {
+		err = rows.Scan(&orderId)
+		if err != nil {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		orderIds = append(orderIds, orderId)
+	}
+
+	orderProducts := map[int][]Product{}
+
+	for _, orderId := range orderIds {
+		rows, err := db.Query(`SELECT products.name,products.image FROM order_products JOIN 
+								products ON order_products.product_id= products.id WHERE order_id =$1`, orderId)
+		if err != nil {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		products := []Product{}
+		for rows.Next() {
+			var name, image string
+			err = rows.Scan(&name, &image)
+			if err != nil {
+				fmt.Println(err)
+				c.AbortWithStatus(http.StatusInternalServerError)
+				return
+			}
+			product := Product{
+				Name:  name,
+				Image: image,
+			}
+			products = append(products, product)
+		}
+		orderProducts[orderId] = products
+	}
+
+	c.JSON(http.StatusOK, orderProducts)
 }
