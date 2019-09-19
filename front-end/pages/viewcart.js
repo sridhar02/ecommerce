cd317f80614ba2b854be97fed6e024a2237bfc5d
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-09-19 14:03:38 +0530

    To get products in cart we have written a class component to fetch from the server and to display in the cart page.

--- front-end/pages/viewcart.js
+++ front-end/pages/viewcart.js
@@ -2,9 +2,74 @@ import React, { Component, Fragment } from "react";
 
 import { Navbar } from "../src/utils";
 
+import Link from "next/link";
+
 import { withStyles } from "@material-ui/core/styles";
 
 import { Button, TextField, Typography } from "@material-ui/core";
+
+const emptyCartStyles = theme => ({
+  image: {
+    textAlign: "center",
+    padding: "30px 0 36px",
+    background: "#fff"
+  },
+  link: {
+    textDecoration: "none",
+    color: "white"
+  },
+  button: {
+    textAlign: "center"
+  }
+});
+
+class _EmptyCart extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Navbar />
+        <Typography className={classes.mycart}>My Cart</Typography>
+        <div className={classes.image}>
+          <img src="/static/cart.png" />
+        </div>
+        <div className={classes.button}>
+          <Button variant="contained" color="primary">
+            <Link href="/products">
+              <a className={classes.link}>SHOP KNOW</a>
+            </Link>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const EmptyCart = withStyles(emptyCartStyles)(_EmptyCart);
+
+const productStyles = theme => ({
+  image: {
+    margin: "10px",
+    height: "100px",
+    width: "120px"
+  }
+});
+
+class _Product extends Component {
+  render() {
+    const { classes, product } = this.props;
+    return (
+      <div>
+        <Typography variant="body2">{product.name}</Typography>
+        <div>
+          <img src={product.image} className={classes.image} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const Product = withStyles(productStyles)(_Product);
 
 const cartStyles = theme => ({
   mycart: {
@@ -17,23 +82,57 @@ const cartStyles = theme => ({
     textAlign: "center",
     padding: "30px 0 36px",
     background: "#fff"
+  },
+  link: {
+    textDecoration: "none"
   }
 });
 
 class _Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: props.products || []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/cart", {
+      method: "GET",
+      headers: {
+        Accept: "applicaton/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("secret")}`
+      }
+    })
+      .then(res => res.json())
+      .then(products => {
+        this.setState({
+          products: products
+        });
+      });
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes, products } = this.props;
+    if (this.state.products.length === 0) {
+      return <EmptyCart />;
+    }
     return (
       <Fragment>
         <Navbar />
         <Typography className={classes.mycart}>My Cart</Typography>
-        <div className={classes.image}>
-          <img src="/static/cart.png" />
-          <div>
-            <Button variant="contained" color="primary">
-              SHOP KNOW
-            </Button>
-          </div>
+        <div>
+          {this.state.products.map(product => (
+            <Product product={product} key={product.id} />
+          ))}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleOrder}
+          >
+            Buy products
+          </Button>
         </div>
       </Fragment>
     );
