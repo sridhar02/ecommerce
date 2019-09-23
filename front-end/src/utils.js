08eca91616c84235ac0c6e60171d960b95224f6d
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-09-23 19:04:24 +0530

    imported Appbar component from materialui and converted navabar in appbar and added some css to look similar

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -9,34 +9,36 @@ import { withStyles } from "@material-ui/styles";
 import { Redirect } from "react-router-dom";
 
 import Router from "next/router";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
 
-const navbarStyles = {
-  navbar: {
-    display: "flex",
-    justifyContent: "space-around",
-    marginBottom: "10px",
-    backgroundColor: "blue"
-  },
-  flipkart: {
-    margin: "0 10px",
-    padding: "10px",
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: "20px"
+const navbarStyles = theme => ({
+  root: {
+    flexGrow: 1,
+    fontFamily: "Roboto,Arial,sans-serif",
+    letterSpacing: "0"
+    // maxWidth: "1248px"
   },
   button: {
-    padding: "10px",
+    padding: theme.spacing(1.5),
     margin: "0 10px",
     fontWeight: "bold",
-    fontSize: "18px",
+    fontSize: theme.spacing(2.5),
     color: "white"
-    // backgroundColor: "blue"
+  },
+  space: {
+    height: "100%",
+    minWidth: "124px"
   },
   searchInput: {
     backgroundColor: "white",
+    margin: "0 auto 0 12px",
     width: "50%"
   }
-};
+});
 
 class _Navbar extends Component {
   constructor(props) {
@@ -49,31 +51,41 @@ class _Navbar extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <nav className={classes.navbar}>
-        <Link href="/products">
-          <img className={classes.flipkart} src="/static/logo.png" />
-        </Link>
-        <TextField
-          id="outlined-bare"
-          margin="normal"
-          variant="outlined"
-          inputProps={{ "aria-label": "bare" }}
-          className={classes.searchInput}
-          placeholder="Search products"
-        />
-        <Link href="/account">
-          <Button className={classes.button}>USER</Button>
-        </Link>
-        <Link href="/viewcart">
-          <Button className={classes.button}>CART</Button>
-        </Link>
-        <Link href="/orders">
-          <Button className={classes.button}>ORDERS</Button>
-        </Link>
-        <Button className={classes.button} onClick={this.onSignout}>
-          SIGNOUT
-        </Button>
-      </nav>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <div className={classes.space}></div>
+            <Link href="/products">
+              <img className={classes.flipkart} src="/static/logo.png" />
+            </Link>
+            <InputBase
+              id="outlined-bare"
+              margin="normal"
+              variant="outlined"
+              inputProps={{ "aria-label": "bare" }}
+              className={classes.searchInput}
+              placeholder="Search products"
+            />
+            <Link href="/account">
+              <Button color="inherit">USER</Button>
+            </Link>
+            <Link href="/viewcart">
+              <Button color="inherit">CART</Button>
+            </Link>
+            <Link href="/orders">
+              <Button color="inherit">ORDERS</Button>
+            </Link>
+            <Button
+              // className={classes.button}
+              color="inherit"
+              onClick={this.onSignout}
+            >
+              SIGNOUT
+            </Button>
+            <div className={classes.space}></div>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
