5e31741dc55270cb82c40290f1f6a78eba35103b
katta sridhar <44333182+sridhar02@users.noreply.github.com>
2019-10-07 11:58:34 +0530

    removed unnecessary css and changed the layout using bootstrap grid responsive.

--- pages/sign_up.js
+++ pages/sign_up.js
@@ -9,33 +9,15 @@ import Link from "next/link";
 import { Button, TextField, Typography } from "@material-ui/core";
 
 import axios from "axios";
+import cx from "classnames";
 
 const signupStyles = theme => ({
-  signupSidebar: {
-    display: "flex",
-    flexDirection: "column",
-    maxWidth: "400px",
-    margin: "0 auto"
-  },
   mainSection: {
-    paddingLeft: "200px",
-    paddingTop: "50px",
-    maxWidth: "1000px",
-    margin: "0 auto",
-    height: "600px",
-    display: "flex",
-    flex: "center"
+    marginTop: theme.spacing(3)
   },
 
-  signupLayout: {
-    margin: "15px",
-    display: "flex",
-    flexDirection: "column",
-    height: "600px"
-  },
   text: {
-    marginTop: "20px",
-    padding: "10px"
+    padding: theme.spacing(2)
   },
 
   signup: {
@@ -47,41 +29,17 @@ const signupStyles = theme => ({
     background: "#fb641b",
     color: "#fff"
   },
-  space1: {
-    padding: "15px"
-  },
   sideBar: {
-    padding: "10px",
-    margin: "15px",
-    backgroundColor: "lightblue",
-    width: "300px",
-    height: "300px",
-    display: "inline-block",
-    flexDirection: "column"
+    backgroundColor: "lightblue"
   },
   navBar: {
-    display: "flex",
-    justifyContent: "space-around",
-    padding: "10px",
-    marginBottom: "10px",
     backgroundColor: "blue"
   },
-
-  searchInput: {
-    height: "36px",
-    width: "50%"
-  },
   button: {
-    padding: "10px",
-    margin: "0 10px",
-    border: "0",
     color: "white",
     fontWeight: "bold",
     fontSize: "18px",
     backgroundColor: "blue"
-  },
-  space1: {
-    padding: "15px"
   }
 });
 
@@ -120,56 +78,75 @@ class _Signup extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.mainSection}>
-        <div className={classes.sideBar}>
-          <span className={classes.signup}>Signup</span>
-          <p className={classes.displayText}>
-            <span>Get access to your Orders, Wishlist and Recommendations</span>
-          </p>
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <div className={classes.signupLayout}>
-            <TextField
-              type="text"
-              name="username"
-              placeholder="Enter your username"
-              value={this.state.username}
-              onChange={this.handleChange}
-              className={classes.loginText}
-            />
-            <TextField
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              className={classes.text}
-            />
-            <TextField
-              name="phonenumber"
-              placeholder="Enter your phone number"
-              value={this.state.phonenumber}
-              onChange={this.handleChange}
-              className={classes.text}
-            />
-            <TextField
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              className={classes.text}
-            />
-            <Button className={classes.signupButton} type="submit">
-              Continue
-            </Button>
-            <div className={classes.space1}>
+      <div className={cx(classes.mainSection, "container")}>
+        <div className="row">
+          <div
+            className={cx(
+              classes.sideBar,
+              "col-md-4 offset-md-2 d-none d-md-block"
+            )}
+          >
+            <span className={classes.signup}>Signup</span>
+            <p className={classes.displayText}>
+              <span>
+                Get access to your Orders, Wishlist and Recommendations
+              </span>
+            </p>
+          </div>
+          <form onSubmit={this.handleSubmit} className="col-12 col-md-4">
+            <div
+              className={cx(classes.signupLayout, "row justify-content-center")}
+            >
+              <TextField
+                type="text"
+                name="username"
+                placeholder="Enter your username"
+                value={this.state.username}
+                onChange={this.handleChange}
+                className={classes.text}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <TextField
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                className={classes.text}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <TextField
+                name="phonenumber"
+                placeholder="Enter your phone number"
+                value={this.state.phonenumber}
+                onChange={this.handleChange}
+                className={classes.text}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <TextField
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                className={classes.text}
+              />
+            </div>
+            <div className="row justify-content-center">
+              <Button className={classes.signupButton} type="submit">
+                Continue
+              </Button>
+            </div>
+            <div className="row justify-content-center">
               <Link href="/login">
                 <a>Existing account/login</a>
               </Link>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
