7e3812d2cfe92da4b7aef048a5ddb9f2b2f1fc98
sridhar02 <kattasridhar02@gmail.com>
2020-06-08 11:21:48 +0530

    hashing password and get user signin route

--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
 require("dotenv").config();
+const PORT = 5000;
+const saltRounds = 10;
+
+// required packages
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const bcrypt = require("bcrypt");
 
 //middleware
-
 app.use(cors());
 app.use(express.json()); //req.body
 
-const PORT = 5000;
+// routes
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.post("/user/signup", async (req, res) => {
+app.post("/user/signup", (req, res) => {
   try {
     const { username, email, password, phonenumber } = req.body;
-    const createUser = await pool.query(
-      `INSERT INTO users(username,email,password,phonenumber,created_at,updated_at)
-      VALUES($1,$2,$3,$4,$5,$6)`,
-      [username, email, password, phonenumber, new Date(), new Date()]
-    );
-    console.log(createUser);
-    res.json("user created sucessfully");
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+      const createUser = await pool.query(
+        `INSERT INTO users(username,email,password,phonenumber,created_at,updated_at)
+        VALUES($1,$2,$3,$4,$5,$6)`,
+        [username, email, hash, phonenumber, new Date(), new Date()]
+      );
+      res.json("user created sucessfully");
+    });
     console.log("user created sucessfully");
   } catch (err) {
     console.error(err.message);
@@ -37,11 +40,12 @@ app.get("/user/sign_in", async (req, res) => {
       `SELECT password,id FROM users WHERE email=$1`,
       [email]
     );
-    if(password === rows.rows[0].password ){
-      res.json("get user sucess")
-    }
-    console.log(rows.rows[0].password);
-    res.json("username or password is wrong");
+    const hash = rows.rows[0].password;
+    bcrypt.compare(password, hash, (err, result) => {
+      result
+        ? res.json("get user sucess")
+        : res.json("email or password is wrong");
+    });
   } catch (err) {
     console.log(err.message);
   }
