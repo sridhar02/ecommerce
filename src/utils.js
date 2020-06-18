a44a26270f5ca6e1e1f934d0102f34e53c33660c
sridhar02 <kattasridhar02@gmail.com>
2020-06-18 07:47:24 +0530

    navbar animation

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-
+import React, { useState, useRef } from "react";
 import Link from "next/link";
-
-import { Button, TextField, Typography } from "@material-ui/core";
-
-import { withStyles } from "@material-ui/styles";
-
-import { Redirect } from "react-router-dom";
-
 import Router from "next/router";
-import { makeStyles } from "@material-ui/core/styles";
+
+import Menu from "@material-ui/core/Menu";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
+import Popover from "@material-ui/core/Popover";
+import { withStyles } from "@material-ui/styles";
+import MenuItem from "@material-ui/core/MenuItem";
 import InputBase from "@material-ui/core/InputBase";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import { Button, TextField, Typography } from "@material-ui/core";
 
 import ShoppingCart from "@material-ui/icons/ShoppingCart";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import Assignment from "@material-ui/icons/Assignment";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import SearchIcon from "@material-ui/icons/Search";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 const navbarStyles = (theme) => ({
   button: {
@@ -39,102 +39,189 @@ const navbarStyles = (theme) => ({
     backgroundColor: "white",
     margin: "8px 0px",
     width: "100%",
-    padding:"0px 20px",
-    borderRadius:"3px"
+    padding: "0px 20px",
+    borderRadius: "3px",
+    // width:"400px"
   },
   login: {
     color: "white",
   },
+  root: {
+    display: "flex",
+  },
 });
 
-class _Navbar extends Component {
-  onSignout = () => {
+const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: "none",
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+  user: {
+    marginTop: "13px",
+  },
+}));
+
+export default function MouseOverPopover({ name }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    console.log(typeof anchorEl);
+    // if (anchorEl === null) {
+    const settime = setTimeout(() => setAnchorEl(null), 500);
+    // }
+    console.log(settime);
+  };
+
+  const open = Boolean(anchorEl);
+  console.log(anchorEl);
+  return (
+    <div>
+      <Typography
+        aria-owns={open ? "mouse-over-popover" : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+        className={classes.user}
+      >
+        {name} {anchorEl ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+      </Typography>
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        // onEntered={handlePopoverOpen}
+        // onClose={handlePopoverClose}
+        disableRestoreFocus
+        // onEnter={handlePopoverOpen}
+      >
+        <Link href="/account">
+          <Typography>My Profile</Typography>
+        </Link>
+        <Typography>SuperCoin Zone </Typography>
+        <Typography>Flipkart Plus Zone</Typography>
+        <Link href="/orders">
+          <Button color="inherit">ORDERS</Button>
+        </Link>
+        <Typography>Wishlist</Typography>
+        <Typography>My Charts</Typography>
+
+        <Typography>Coupons</Typography>
+        <Typography>Gift Cards</Typography>
+        <Typography>notifications</Typography>
+        <div>
+          <Button>SIGNOUT</Button>
+        </div>
+      </Popover>
+    </div>
+  );
+}
+
+function _Navbar({ classes, search, setSearch }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const onSignout = () => {
     localStorage.removeItem("secret");
     Router.push("/login");
   };
-  render() {
-    const { classes, search, setSearch } = this.props;
-    let authorized;
-    let iconAuthorized;
-    if (typeof window !== "undefined" && localStorage.getItem("secret")) {
-      authorized = (
-        <div className="d-none d-md-block col-md-6">
-          <Link href="/account">
-            <Button color="inherit">USER</Button>
-          </Link>
-          <Link href="/viewcart">
-            <Button color="inherit">CART</Button>
-          </Link>
-          <Link href="/orders">
-            <Button color="inherit">ORDERS</Button>
-          </Link>
-          <Button color="inherit" onClick={this.onSignout}>
-            SIGNOUT
-          </Button>
-        </div>
-      );
-      iconAuthorized = (
-        <div className="d-md-none">
-          <Link href="/account">
-            <IconButton color="inherit">
-              <AccountCircle />
-            </IconButton>
-          </Link>
-          <Link href="/viewcart">
-            <IconButton color="inherit">
-              <ShoppingCart />
-            </IconButton>
-          </Link>
-          <Link href="/orders">
-            <IconButton color="inherit">
-              <Assignment />
-            </IconButton>
-          </Link>
-          <IconButton color="inherit">
-            <ExitToApp />
-          </IconButton>
-        </div>
-      );
-    } else {
-      authorized = (
-        <div>
-          <Link href="/login">
-            <Button className={classes.login}>login</Button>
-          </Link>
-        </div>
-      );
-    }
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <div className="container">
-            <div className="row">
-              <div className="col-4 col-md-2">
-                <Link href="/products">
-                  <img className={classes.flipkart} src="/static/logo.png" />
-                </Link>{" "}
-              </div>
-              {iconAuthorized}
-              <div className="col-12 col-md-4">
-                <InputBase
-                  id="outlined-bare"
-                  variant="outlined"
-                  inputProps={{ "aria-label": "bare" }}
-                  className={classes.searchInput}
-                  placeholder="Search products"
-                  value={search}
-                  onChange={setSearch}
-                  endAdornment={<SearchIcon />}
-                />
-              </div>
-              {authorized}
-            </div>
-          </div>
-        </AppBar>
+  let authorized;
+  let iconAuthorized;
+  if (typeof window !== "undefined" && localStorage.getItem("secret")) {
+    authorized = (
+      <div className="d-none d-md-block col-md-6" className={classes.root}>
+        <MouseOverPopover name={"username"} />
+        <Link href="/viewcart">
+          <Button color="inherit">
+            <ShoppingCart />
+            CART
+          </Button>
+        </Link>
+        <Button color="inherit" onClick={onSignout}>
+          SIGNOUT
+        </Button>
+      </div>
+    );
+    iconAuthorized = (
+      <span className="d-md-none">
+        <Link href="/account">
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Link>
+        <Link href="/viewcart">
+          <IconButton color="inherit">
+            <ShoppingCart />
+          </IconButton>
+        </Link>
+        <Link href="/orders">
+          <IconButton color="inherit">
+            <Assignment />
+          </IconButton>
+        </Link>
+        <IconButton color="inherit">
+          <ExitToApp />
+        </IconButton>
+      </span>
+    );
+  } else {
+    authorized = (
+      <div>
+        <Link href="/login">
+          <Button className={classes.login}>login</Button>
+        </Link>
       </div>
     );
   }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <div className="container">
+          <div className="row">
+            <div className="col-4 col-md-2">
+              <Link href="/products">
+                <img className={classes.flipkart} src="/static/logo.png" />
+              </Link>{" "}
+            </div>
+            {iconAuthorized}
+            <div className="col-12 col-md-4">
+              <InputBase
+                id="outlined-bare"
+                variant="outlined"
+                inputProps={{ "aria-label": "bare" }}
+                className={classes.searchInput}
+                placeholder="Search products"
+                value={search}
+                onChange={setSearch}
+                endAdornment={<SearchIcon />}
+              />
+            </div>
+            {authorized}
+          </div>
+        </div>
+      </AppBar>
+    </div>
+  );
 }
 
 export const Navbar = withStyles(navbarStyles)(_Navbar);
